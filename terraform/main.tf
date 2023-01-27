7b4fe20f1c5224536c19ee9625b1ebdc0611a326
sush-19 <114879514+sush-19@users.noreply.github.com>
2023-01-27 01:06:43 +0000

    save

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "dev_server" {
 ami = var.ami_id
 instance_type = var.instance_type 
 key_name = var.key_name
-security_groups = "mono-project"
 tags = {
 Name = var.name
 Environment = "dev"
